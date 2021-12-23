cf79adb3607085a26ce8484f17bdbd15936e8222
Michael Karamuth <michael@arsero.com>
2021-12-23 13:21:33 +0400

    feature: Utilities for captcha validation

--- allcaptcha/tests.py
+++ allcaptcha/tests.py
@@ -1,3 +1,131 @@
+import secrets
+from unittest.mock import patch, MagicMock
+from django import forms
 from django.test import TestCase
 
-# Create your tests here.
+from allcaptcha.mixins import HCaptchaFormMixin
+from . import settings
+from . import utils
+
+
+class AForm(forms.Form):
+    pass
+
+
+class ACaptchaedForm(HCaptchaFormMixin, AForm):
+    pass
+
+
+class AllCaptchaTests(TestCase):
+    def test_get_captcha_response(self):
+
+        form = ACaptchaedForm({"h-captcha-response": "abcd"})
+        captcha_response = utils.get_captcha_response(form)
+        self.assertEqual(captcha_response, "abcd")
+
+        form = ACaptchaedForm({})
+        captcha_response = utils.get_captcha_response(form)
+        self.assertEqual(len(captcha_response), 0)
+
+    def test_build_submission_data(self):
+
+        with patch("allcaptcha.utils.settings") as mock_settings:
+
+            # Test with HCaptcha
+            mock_settings.HCAPTCHA_PROVIDER_NAME = settings.HCAPTCHA_PROVIDER_NAME
+            mock_settings.PROVIDER = settings.HCAPTCHA_PROVIDER_NAME
+            mock_settings.CAPTCHA_SECRET_KEY = settings.HCAPTCHA_SECRET_KEY
+            mock_settings.CAPTCHA_SITE_KEY = settings.HCAPTCHA_SITE_KEY
+            data = utils._build_submission_data("abcd")
+
+            self.assertEqual(data["secret"], settings.HCAPTCHA_SECRET_KEY)
+            self.assertEqual(data["sitekey"], settings.HCAPTCHA_SITE_KEY)
+            self.assertEqual(data["response"], "abcd")
+
+            # Test with non-existent provider
+
+            mock_settings.HCAPTCHA_PROVIDER_NAME = secrets.token_hex(4)
+            data = utils._build_submission_data("abcd")
+
+            self.assertEqual(len(data.keys()), 0)
+
+    def test_determine_success(self):
+
+        response = None
+
+        success = utils._determine_success(response)
+
+        self.assertFalse(success)
+
+        response = MagicMock()
+
+        response.json.return_value = {"success": True}
+
+        response.status_code = 200
+
+        success = utils._determine_success(response)
+
+        self.assertTrue(success)
+
+        response.json.return_value = {"success": False}
+
+        success = utils._determine_success(response)
+
+        self.assertFalse(success)
+
+        response.json.return_value = {"success": True}
+
+        response.status_code = 400
+
+        self.assertFalse(success)
+
+        response.status_code = 200
+
+        response.json.side_effect = Exception
+
+        self.assertFalse(success)
+
+    def test_get_provider_response(self):
+
+        with patch("allcaptcha.utils.requests") as mock_requests:
+
+            mock_requests.post.return_value = MagicMock()
+
+            self.assertIsInstance(utils._get_provider_response({}), MagicMock)
+
+            mock_requests.post.side_effect = Exception
+
+            self.assertIsNone(utils._get_provider_response({}))
+
+    def test_valid_response(self):
+
+        with patch("allcaptcha.utils.requests") as mock_requests:
+            response = MagicMock()
+            response.status_code = 200
+            response.json.return_value = {"success": True}
+            mock_requests.post.return_value = response
+
+            self.assertTrue(utils.valid_response("abcd"))
+
+            self.assertFalse(utils.valid_response(""))
+
+            response = MagicMock()
+            response.status_code = 400
+            response.json.return_value = {"success": True}
+            mock_requests.post.return_value = response
+
+            self.assertFalse(utils.valid_response("abcd"))
+
+            response = MagicMock()
+            response.status_code = 200
+            response.json.return_value = {"success": False}
+            mock_requests.post.return_value = response
+
+            self.assertFalse(utils.valid_response("abcd"))
+
+            response = MagicMock()
+            response.status_code = 400
+            response.json.return_value = {"success": False}
+            mock_requests.post.return_value = response
+
+            self.assertFalse(utils.valid_response("abcd"))
---
+++ allcaptcha/utils.py
@@ -0,0 +1,66 @@
+from typing import Optional
+
+import requests
+import logging
+
+from django.forms import BaseForm
+from requests import Response
+
+from . import settings
+
+log = logging.getLogger(__name__)
+
+
+def get_captcha_response(form: BaseForm) -> str:
+    captcha_response = ""
+    if settings.PROVIDER == settings.HCAPTCHA_PROVIDER_NAME:
+        captcha_response = form.data.get("h-captcha-response", "")
+    return captcha_response
+
+
+def _build_submission_data(response: str) -> dict:
+    data = {}
+    if settings.PROVIDER == settings.HCAPTCHA_PROVIDER_NAME:
+        data.update(
+            {
+                "secret": settings.CAPTCHA_SECRET_KEY,
+                "sitekey": settings.CAPTCHA_SITE_KEY,
+                "response": response,
+            }
+        )
+
+    return data
+
+
+def _determine_success(provider_response: Optional[Response]) -> bool:
+    success = False
+    if provider_response and getattr(provider_response, "status_code", None) == 200:
+        if settings.PROVIDER == settings.HCAPTCHA_PROVIDER_NAME:
+            try:
+                success = provider_response.json()["success"]
+            except Exception as e:
+                log.exception(e)
+    return success
+
+
+def _get_provider_response(data: dict) -> Optional[Response]:
+    provider_response = None
+    if settings.PROVIDER == settings.HCAPTCHA_PROVIDER_NAME:
+        try:
+            provider_response = requests.post(settings.PROVIDER_URL, data=data)
+        except Exception as e:
+            log.exception(e)
+    return provider_response
+
+
+def valid_response(response: str) -> bool:
+    success = False
+
+    if len(response) > 0:
+        data = _build_submission_data(response)
+
+        provider_response = _get_provider_response(data)
+
+        success = _determine_success(provider_response)
+
+    return success
