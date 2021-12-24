c0ce9cd90bfbf23ae94f8dcaa793267fd9a01885
Michael Karamuth <michael@arsero.com>
2021-12-24 12:01:22 +0400

    feature: Config to specify external js file for captcha service

--- allcaptcha/settings.py
+++ allcaptcha/settings.py
@@ -14,6 +14,8 @@ HCAPTCHA_URL = getattr(settings, "HCAPTCHA_URL", "https://hcaptcha.com/siteverif
 HCAPTCHA_SECRET_KEY = getattr(settings, "HCAPTCHA_SECRET_KEY", None)
 HCAPTCHA_SITE_KEY = getattr(settings, "HCAPTCHA_SITE_KEY", None)
 
+HCAPTCHA_JS = getattr(settings, "HCAPTCHA_JS", "https://js.hcaptcha.com/1/api.js")
+
 if not HCAPTCHA_SECRET_KEY:
     raise_for_attr("HCAPTCHA_SECRET_KEY")
 
@@ -26,6 +28,7 @@ if PROVIDER == HCAPTCHA_PROVIDER_NAME:
     CAPTCHA_SITE_KEY = HCAPTCHA_SITE_KEY
     CAPTCHA_SECRET_KEY = HCAPTCHA_SECRET_KEY
     PROVIDER_URL = HCAPTCHA_URL
+    PROVIDER_JS = HCAPTCHA_JS
 else:
     raise ImproperlyConfigured(
         _(f'Provider  "{PROVIDER}" hasn\'t been implemented yet.')
