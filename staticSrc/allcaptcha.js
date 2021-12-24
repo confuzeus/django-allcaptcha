cb19fa34b2132493cc5e2ae619ac92ea3cb228c5
Michael Karamuth <michael@arsero.com>
2021-12-24 16:08:15 +0400

    refactor: Simplify client script

--- staticSrc/allcaptcha.js
+++ staticSrc/allcaptcha.js
@@ -1,21 +1,27 @@
-var allcaptcha = (function(o) {
-    o.main = {
-        onHcaptchaSubmit: function onHcaptchaSubmit(token) {
-            const form = document.querySelector("form");
-            const responseInput = document.createElement("input");
-            responseInput.setAttribute("type", "hidden");
-            responseInput.setAttribute("name", "h-captcha-response");
-            responseInput.setAttribute("value", token)
-            form.appendChild(responseInput);
-            form.submit();
-        },
-        init: function initAllCaptcha() {
-            window.onHcaptchaSubmit = this.onHcaptchaSubmit;
-        }
+(function () {
+
+    const form = document.querySelector("form");
+
+    function appendToForm(name, token) {
+        const responseInput = document.createElement("input");
+        responseInput.setAttribute("type", "hidden");
+        responseInput.setAttribute("name", name);
+        responseInput.setAttribute("value", token);
+        form.appendChild(responseInput);
+    }
+
+    function onHcaptchaSubmit(token) {
+        appendToForm("h-captcha-response", token);
+        form.submit();
+    }
+
+    function onRecaptchaSubmit(token) {
+        appendToForm("g-recaptcha-response", token);
+        form.submit();
     }
 
     window.addEventListener("DOMContentLoaded", function () {
-        o.main.init();
+        window.onHcaptchaSubmit = onHcaptchaSubmit;
+        window.onRecaptchaSubmit = onRecaptchaSubmit;
     });
-    return o;
-})(allcaptcha || {});
+})();
