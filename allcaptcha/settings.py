930a81755bd712bc52c5554fd57dfa5b0c92e829
Michael Karamuth <michael@arsero.com>
2021-12-24 14:41:23 +0400

    refactor: Only raise if provider is h-captcha

--- allcaptcha/settings.py
+++ allcaptcha/settings.py
@@ -17,15 +17,15 @@ HCAPTCHA_SITE_KEY = getattr(settings, "HCAPTCHA_SITE_KEY", None)
 HCAPTCHA_JS = getattr(settings, "HCAPTCHA_JS", "https://js.hcaptcha.com/1/api.js")
 HCAPTCHA_JS_CALLBACK = getattr(settings, "HCAPTCHA_JS_CALLBACK", "onHcaptchaSubmit")
 
-if not HCAPTCHA_SECRET_KEY:
-    raise_for_attr("HCAPTCHA_SECRET_KEY")
-
-if not HCAPTCHA_SITE_KEY:
-    raise_for_attr("HCAPTCHA_SITE_KEY")
 
 PROVIDER = getattr(settings, "CAPTCHA_PROVIDER", HCAPTCHA_PROVIDER_NAME)
 
 if PROVIDER == HCAPTCHA_PROVIDER_NAME:
+    if not HCAPTCHA_SECRET_KEY:
+        raise_for_attr("HCAPTCHA_SECRET_KEY")
+
+    if not HCAPTCHA_SITE_KEY:
+        raise_for_attr("HCAPTCHA_SITE_KEY")
     CAPTCHA_SITE_KEY = HCAPTCHA_SITE_KEY
     CAPTCHA_SECRET_KEY = HCAPTCHA_SECRET_KEY
     PROVIDER_URL = HCAPTCHA_URL
