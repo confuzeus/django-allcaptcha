8ad48490b407b0bd1d9613872e03a8ada0d60165
Michael Karamuth <michael@arsero.com>
2021-12-27 19:57:36 +0400

    fix: Set long descrption to Markdown

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     install_requires=["requests"],
     license="MIT license",
     long_description=readme,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="django-allcaptcha",
     name="django-allcaptcha",
